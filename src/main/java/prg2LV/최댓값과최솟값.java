733f13ab96bcdc00294b5848ac8f8963688e62c8
unknown <wnsrl981228@gmail.com>
2022-11-29 16:39:11 +0900

    최댓값과최솟값

---
+++ src/main/java/prg2LV/최댓값과최솟값.java
@@ -0,0 +1,20 @@
+package prg2LV;
+
+public class 최댓값과최솟값 {
+    public String solution(String s) {
+        String[] s1 = s.split(" ");
+        int max = Integer.parseInt(s1[0]);
+        int min = Integer.parseInt(s1[0]);
+
+        for (int i = 1; i < s1.length; i++) {
+            int num = Integer.parseInt(s1[i]);
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min+" "+max;
+    }
+}
