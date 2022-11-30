e29d229db227e46ff31573d272a3d0791d5c21a8
unknown <wnsrl981228@gmail.com>
2022-11-30 14:57:15 +0900

    숫자의표현

---
+++ src/main/java/prg2LV/숫자의표현.java
@@ -0,0 +1,30 @@
+package prg2LV;
+
+public class 숫자의표현 {
+    public static void main(String[] args) {
+        int solution = solution(3);
+        System.out.println("solution = " + solution);
+    }
+    // 정답은 n의 홀수인 약수 개수와 같다.
+    public static int solution(int n) {
+        int answer = 1;
+        for (int i = 1; i < n/2 + 1; i++) {
+            int sum = 0;
+            for (int j = i; j <= n/2 + 1 ; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                if (sum > n) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
+/**
+ * 연속된 자연수들로 n을 표현하는 방법의 수
+ */
+
