551289c32718142868873a63fd1cf39aeed9b55e
unknown <wnsrl981228@gmail.com>
2022-11-30 17:20:32 +0900

    카펫

---
+++ src/main/java/prg2LV/카펫.java
@@ -0,0 +1,25 @@
+package prg2LV;
+
+public class 카펫 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        int sum = (brown / 2) + 2; // 가로 + 세로
+        for (int h = 3; h <= sum/2; h++) {
+            int w = sum - h;
+            int yellowSize = (w - 2) * (h - 2);
+            if (yellow == yellowSize) {
+                answer[0] = w;
+                answer[1] = h;
+            }
+        }
+
+        return answer;
+    }
+}
+
+/**
+ * 갈색의 최소 가로 세로는 3
+ * 노랑의 넓이 = (갈색의 가로 - 2) * (갈색의 세로 - 2)
+ * 가로 >= 세로
+ */
