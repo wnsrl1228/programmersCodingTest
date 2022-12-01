7f0f386b8e6093b4f320d3b215d7433e027950cd
unknown <wnsrl981228@gmail.com>
2022-12-01 14:01:22 +0900

    짝지어제거하기

---
+++ src/main/java/prg2LV/짝지어제거하기.java
@@ -0,0 +1,52 @@
+package prg2LV;
+
+import java.util.Stack;
+
+public class 짝지어제거하기 {
+    public static void main(String[] args) {
+        solution("baabaa");
+    }
+    // 최적화
+    public static int solution1(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.add(c);
+            } else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.add(c);
+                }
+            }
+        }
+        return stack.size() > 0 ? 0 : 1;
+    }
+    public static int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.add(c);
+            } else {
+                char tmp = stack.peek();
+                if (tmp == c) {
+                    stack.pop();
+                } else {
+                    stack.add(c);
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return 1;
+        }
+        return 0;
+    }
+}
+/**
+ * 같은 알파벳 2개 붙어있는 걸 제거
+ * 문자가 모두 제거되면 1
+ * 아닐 경우 0
+ */
