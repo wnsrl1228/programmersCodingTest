297cfa24a89418d431b463387ad772bd8270daf9
unknown <wnsrl981228@gmail.com>
2022-12-02 13:57:09 +0900

    행렬의곱셈

---
+++ src/main/java/prg2LV/행렬의곱셈.java
@@ -0,0 +1,17 @@
+package prg2LV;
+
+public class 행렬의곱셈 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[0].length; j++) {
+                for (int k = 0; k < arr1[i].length; k++) {
+                    answer[i][j]+= arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
+
