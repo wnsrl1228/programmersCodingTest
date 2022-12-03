4a6275ea9648f57eaec538a8c72eac62acf863f3
unknown <wnsrl981228@gmail.com>
2022-12-03 20:08:57 +0900

    타겟넘버

---
+++ src/main/java/prg2LV/타겟넘버.java
@@ -0,0 +1,20 @@
+package prg2LV;
+
+public class 타겟넘버 {
+    
+    public static int result = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0,0);
+        return result;
+    }
+
+    private void dfs(int[] numbers, int target, int index, int sum) {
+        if (index >= numbers.length) {
+            if (sum == target) result++;
+            return;
+        }
+        int i = index;
+        dfs(numbers, target, i+1, sum+numbers[i]);
+        dfs(numbers, target, i+1, sum-numbers[i]);
+    }
+}
