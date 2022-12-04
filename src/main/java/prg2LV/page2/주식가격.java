cb0388b60ab47d8aa5bbe086ff01571eb50a51ba
unknown <wnsrl981228@gmail.com>
2022-12-04 17:59:42 +0900

    주식가격

---
+++ src/main/java/prg2LV/page2/주식가격.java
@@ -0,0 +1,16 @@
+package prg2LV.page2;
+
+public class 주식가격 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[j] < prices[i]) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
