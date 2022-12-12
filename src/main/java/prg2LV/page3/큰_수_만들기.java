c30bfdfabc85b62b2e7ba70b0f12affda3cb5353
unknown <wnsrl981228@gmail.com>
2022-12-12 19:11:05 +0900

    큰_수_만들기

---
+++ src/main/java/prg2LV/page3/큰_수_만들기.java
@@ -0,0 +1,52 @@
+package prg2LV.page3;
+
+import java.util.Stack;
+
+// 못 품
+public class 큰_수_만들기 {
+    public static void main(String[] args) {
+        solution("1231234",3);
+    }
+
+    // 스택을 이용한 풀이
+    public String solution1(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }
+    // 범위 안에 가장 큰 값만 answer 에 추가하는 풀이
+    public static String solution(String number, int k) {
+
+        StringBuilder answer = new StringBuilder();
+
+        int len = number.length() - k;
+        int start = 0;
+        int i = 1;
+
+        while (start < number.length() && answer.length() != len) {
+            int leftNum = number.length() - len + i;
+            int max = 0;
+            for (int j = start; j < leftNum ; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            answer.append(max);
+            i++;
+        }
+
+        return answer.toString();
+    }
+}
