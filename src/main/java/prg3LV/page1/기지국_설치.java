6b69c3be54a2259fd1e1f55671e8f4def1234b33
unknown <wnsrl981228@gmail.com>
2022-12-27 16:49:08 +0900

    기지국_설치 수정

--- src/main/java/prg3LV/page1/기지국_설치.java
+++ src/main/java/prg3LV/page1/기지국_설치.java
@@ -1,25 +1,18 @@
 package prg3LV.page1;
 
 public class 기지국_설치 {
-    public static void main(String[] args) {
-        System.out.println(4%3);
-    }
+
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-
-
         int start = 0;
-        double range = w*2+1;
+        int range = w*2+1;
         for (int station : stations) {
-            int stat = station - 1;
-            int apartmentCount = (stat - w) - start;
-            answer += Math.ceil(apartmentCount/ range);
-            start = stat + w + 1;
+            int width = ((station - 1 - w) - start);
+            answer += width % range > 0 ? width / range + 1 : width / range;
+            start = station + w;
         }
-        if (start < n) {
-            int remain = n - start;
-            answer += Math.ceil(remain/range);
-        }
+        int width = n - start;
+        answer += width % range > 0 ? width / range + 1 : width / range;
         return answer;
     }
 }
